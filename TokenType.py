445c8bc583a47787bd579b808c007424ff292dc7
Hareesh Lakshmi Narayanan <hareeshlakshminarayanan@hareeshs-mbp.lan>
2024-01-04 15:37:42 -0800

    Add Scanner support for PRINT keyword and STRING literal

--- TokenType.py
+++ TokenType.py
@@ -2,16 +2,10 @@ from enum import Enum, auto
 
 
 class TokenType(Enum):
-    # Single character tokens
-    LEFT_PARENTHESIS = auto()
-    RIGHT_PARENTHESIS = auto()
-    LEFT_BRACKET = auto()
-    RIGHT_BRACKET = auto()
-    COMMA = auto()
-    DOT = auto()
-    MINUS = auto()
-    PLUS = auto()
-    SEMICOLON = auto()
-    SLASH = auto()
+    #Keywords
+    PRINT = auto()
 
+    #Literals
+    IDENTIFIER = auto()
+    STRING = auto()
     EOF = auto()
